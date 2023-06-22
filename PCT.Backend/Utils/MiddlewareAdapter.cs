499359326938637215473768cd2ed5141313ef19
Ken Kirui <31723768+kenkirui@users.noreply.github.com>
2023-06-22 16:11:55 +0300

    Update MiddlewareAdapter

--- PCT.Backend/Utils/MiddlewareAdapter.cs
+++ PCT.Backend/Utils/MiddlewareAdapter.cs
@@ -6,6 +6,68 @@ namespace PCT.Backend.Utils
 {
     public static class MiddlewareAdapter
     {
+        public static ServiceBusClient client = new ServiceBusClient("Endpoint=sb://gscih-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q9JB5dtPzAeBSRcnlf+PxEj1MQbEjpaeL+ASbCzVV7g=");
+
+        public static async void PostLocationToMiddleWare(Location location)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(location);
+                ServiceBusSender sender = client.CreateSender("location");
+                await PostContentToMiddleware(client, sender, json);
+                Console.WriteLine("Location " + location.Name + ": posted successfully to middleware");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Location " + location.Name + ": " + e.Message);
+            }
+        }
+
+        public static async void PostCarrierToMiddleWare(Carrier carrier)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(carrier);
+                ServiceBusSender sender = client.CreateSender("carrier");
+                await PostContentToMiddleware(client, sender, json);
+                Console.WriteLine("Carrier " + carrier.Name + ": posted successfully to middleware");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Carrier " + carrier.Name + ": " + e.Message);
+            }
+        }
+
+        public static async void PostProductToMiddleWare(Product product)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(product);
+                ServiceBusSender sender = client.CreateSender("product");
+                await PostContentToMiddleware(client, sender, json);
+                Console.WriteLine("Product " + product.Name + ": posted successfully to middleware");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Product " + product.Name + ": " + e.Message);
+            }
+        }
+
+        public static async void PostVendorToMiddleWare(Vendor vendor)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(vendor);
+                ServiceBusSender sender = client.CreateSender("vendor");
+                await PostContentToMiddleware(client, sender, json);
+                Console.WriteLine("Vendor " + vendor.Name + ": posted successfully to middleware");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Vendor " + vendor.Name + ": " + e.Message);
+            }
+        }
+
         private static async Task<string> PostContentToMiddleware(ServiceBusClient client, ServiceBusSender sender, string content)
         {
             try
@@ -28,74 +90,6 @@ namespace PCT.Backend.Utils
             catch
             {
                 throw;
-            }
-        }
-
-        public static async void PostLocationToMiddleWare(Location location)
-        {
-            try
-            {
-                var productjson = JsonConvert.SerializeObject(location);
-
-                ServiceBusClient client = new ServiceBusClient("Endpoint=sb://gscih-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q9JB5dtPzAeBSRcnlf+PxEj1MQbEjpaeL+ASbCzVV7g=");
-                ServiceBusSender sender = client.CreateSender("location");
-                await PostContentToMiddleware(client, sender, productjson);
-                Console.WriteLine("Location " + location.Name + ": posted successfully to middleware");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Location " + location.Name + ": " + e.Message);
-            }
-        }
-
-        public static async void PostCarrierToMiddleWare(Carrier carrier)
-        {
-            try
-            {
-                var productjson = JsonConvert.SerializeObject(carrier);
-
-                ServiceBusClient client = new ServiceBusClient("Endpoint=sb://gscih-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q9JB5dtPzAeBSRcnlf+PxEj1MQbEjpaeL+ASbCzVV7g=");
-                ServiceBusSender sender = client.CreateSender("carrier");
-                await PostContentToMiddleware(client, sender, productjson);
-                Console.WriteLine("Carrier " + carrier.Name + ": posted successfully to middleware");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Carrier " + carrier.Name + ": " + e.Message);
-            }
-        }
-
-        public static async void PostProductToMiddleWare(Product product)
-        {
-            try
-            {
-                var productjson = JsonConvert.SerializeObject(product);
-
-                ServiceBusClient client = new ServiceBusClient("Endpoint=sb://gscih-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q9JB5dtPzAeBSRcnlf+PxEj1MQbEjpaeL+ASbCzVV7g=");
-                ServiceBusSender sender = client.CreateSender("product");
-                await PostContentToMiddleware(client, sender, productjson);
-                Console.WriteLine("Product " + product.Name + ": posted successfully to middleware");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Product " + product.Name + ": " + e.Message);
-            }
-        }
-
-        public static async void PostVendorToMiddleWare(Vendor vendor)
-        {
-            try
-            {
-                var productjson = JsonConvert.SerializeObject(vendor);
-
-                ServiceBusClient client = new ServiceBusClient("Endpoint=sb://gscih-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q9JB5dtPzAeBSRcnlf+PxEj1MQbEjpaeL+ASbCzVV7g=");
-                ServiceBusSender sender = client.CreateSender("vendor");
-                await PostContentToMiddleware(client, sender, productjson);
-                Console.WriteLine("Vendor " + vendor.Name + ": posted successfully to middleware");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Vendor " + vendor.Name + ": " + e.Message);
             }
         }
     }
